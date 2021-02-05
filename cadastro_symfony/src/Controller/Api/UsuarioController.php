767748c0e27192e28b1ac71219a36a598f77a3d0
josiassoares <61014395+josiassoares@users.noreply.github.com>
2021-02-05 18:41:45 -0300

    Finalizando API

--- cadastro_symfony/src/Controller/Api/UsuarioController.php
+++ cadastro_symfony/src/Controller/Api/UsuarioController.php
@@ -1,28 +1,53 @@
 <?php
 
 namespace App\Controller\Api;
+use App\Entity\Usuario;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/api/v1", name="api_v1_usuario_")
  */
-class UsuarioController
+class UsuarioController extends AbstractController
 {
     /**
      * @Route("/lista", methods={"GET"}, name="lista")
      */
     public function lista() : JsonResponse
     {
-        return new JsonResponse(["implementar lista na API", 404]);
+        $doctrine = $this->getDoctrine()->getRepository(Usuario::class);
+
+        return new JsonResponse($doctrine->obterTodosRegistros());
     }
 
 
     /**
      * @Route("/cadastra", methods={"POST"}, name="cadastra")
+     * @param Request $request
+     * @return Response
      */
-    public function cadastra() : JsonResponse
+    public function cadastra(Request $request) : Response
     {
-        return new JsonResponse(["implementar cadastro API", 404]);
+        $data = $request->request->all();
+
+        $usuario = new Usuario;
+        $usuario->setNome($data['nome']);
+        $usuario->setEmail($data['email']);
+
+        $doctrine = $this->getDoctrine()->getManager();
+        $doctrine->persist($usuario);
+        $doctrine->flush();
+
+        if($doctrine->contains($usuario))
+        {
+            return new Response("ok", 200);
+        }
+        else
+        {
+            return new Response("erro", 404);
+        }
     }
 }
